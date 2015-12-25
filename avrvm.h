5ad98cd975db9cb4db7f05b1acbcd85044884c59
jkmnt <firewood@mail.ru>
2015-12-25 14:35:52 +0300

    * test code removal, cosmetic

--- avrvm.h
+++ avrvm.h
@@ -2,26 +2,35 @@
 #define __AVRVM_H__
 
 #define AVRVM_N_IO_REGS 61
-// 32k words, 64kB of flash
+
+/* 32k words, 64kB of flash are supported for code. Addresses above 32k are treated as a table of ROMed functions */
 #define AVRVM_MAX_FLASH_WORDS 32768
-// 32k -- 65k addresses are kind of ROMed external functions
 
 enum avrvm_rc_e
 {
+    /* Failed to decode instruction */
     AVRVM_RC_UNDEF_INSTR = -5,
+    /* Watchdog reset. Useful if host is applying strict deadlines to script */
     AVRVM_RC_WDR = -4,
+    /* 'break' assembly instruction was decoded. May be treated as a script 'exit' */
     AVRVM_RC_BREAK = -3,
+    /* Script got nothing to do and intending to nap until external host event */
     AVRVM_RC_SLEEP = -2,
+    /* Usual return value */
     AVRVM_RC_OK = -1,
-    AVRVM_RC_EXT_CALL = 0,
 };
 
 typedef struct
 {
+    /* called for fetching instruction (or flash data) word. Argument is a word (not byte) offset from the flash base */
     uint16_t (*flash_r)(uint16_t word_idx);
+    /* called for reading a byte from sram at the specified address */
     uint8_t (*sram_r)(uint16_t addr);
+    /* called for writing a byte to sram at the specified address */
     void (*sram_w)(uint16_t addr, uint8_t byte);
+    /* called for reading a byte from the specified i/o register (0 .. 61) */
     uint8_t (*io_r)(uint8_t addr);
+    /* called for writing a byte to the specified i/o register (0 .. 61 )*/
     void (*io_w)(uint8_t addr, uint8_t byte);
 } avrvm_iface_t;
 
@@ -48,10 +57,66 @@ typedef struct
     };
 } avrvm_ctx_t;
 
+/*!
+ * \brief Init avrvm
+ *
+ * \param ctx - VM context instance
+ * \param iface - iface specification
+ * \param sram_size - sram size. Used for setting initial stack pointer only
+ */
 void avrvm_init(avrvm_ctx_t *ctx, const avrvm_iface_t *iface, uint16_t sram_size);
+
+/*!
+ * \brief Execute a single instruction of script
+ *
+ * \param ctx - VM context instance
+ *
+ * \return negative return code: one of avrvm_rc_e, non-negative: index of called host API function
+ */
 int avrvm_exec(avrvm_ctx_t *ctx);
 
+/*!
+ * \brief Helper for unpacking function call arguments according to the avr-gcc call convention
+ *
+ * \param ctx - VM context instance
+ * \param fmt - format string specifying ordered arguments
+ * \param ... - pointers to variables to store unpacked arguments (similar to scanf)
+ *
+ * Recognized characters from format string:
+ * c - char
+ * b - int8_t
+ * B - uint8_t
+ * h - int16_t
+ * H - uint16_t
+ * i, l - int32_t
+ * I, L - uint32_t
+ * f - float (4 byte)
+ * q - int64_t
+ * Q - uint64_t
+ * P - void * pointer in script's memory space, converted to uint16_t offset from the beginning of the host-emulated sram
+ */
 void avrvm_unpack_args_gcc(const avrvm_ctx_t *ctx, const char *fmt, ...);
+
+/*!
+ * \brief Helper for packing function return value according to the avr-gcc call convention
+ *
+ * \param ctx - VM context instance
+ * \param fmt - format character
+ * \param val - pointer to variable to return
+ *
+ * Recognized format characters:
+ * c - char
+ * b - int8_t
+ * B - uint8_t
+ * h - int16_t
+ * H - uint16_t
+ * i, l - int32_t
+ * I, L - uint32_t
+ * f - float (4 byte)
+ * q - int64_t
+ * Q - uint64_t
+ * P - uint16_t offset from the beginning of the host-emulated sram. converted to void * pointer in script's memory space  
+ */
 void avrvm_pack_return_gcc(avrvm_ctx_t *ctx, char fmt, const void *val);
 
 #endif
--- hello/host_hello.c
+++ hello/host_hello.c
@@ -7,6 +7,7 @@
 #include <conio.h>      // for _kbhit and _getch
 
 static FILE *src;
+static uint8_t counter;
 
 static avrvm_ctx_t vm;
 static uint8_t sram[1024 + 160];
@@ -74,173 +75,17 @@ static void exec_ext_call(int idx)
 
     case 2:
         {
-            uint16_t a;
-            uint32_t b;
-            uint32_t c;
-            uint8_t d;
-            uint16_t e;
-            uint8_t f;
-            uint8_t g;
-            avrvm_unpack_args_gcc(&vm, "HIIBHBB", &a, &b, &c, &d, &e, &f, &g);
-
-            printf("a = %d "
-                   "b = %d "
-                   "c = %d "
-                   "d = %d "
-                   "e = %d "
-                   "f = %d "
-                   "g = %d \n", a, b, c, d, e, f, g);
+            avrvm_pack_return_gcc(&vm, 'B', &counter);
+            counter++;
         }
-        break;
-
-    case 3:
-        {
-            uint16_t a;
-            uint32_t b;
-            uint32_t c;
-            uint8_t d;
-            uint16_t e;
-            uint8_t f;
-            uint32_t g;
-            uint16_t h;
-            uint8_t i;
-            uint8_t j;
-            avrvm_unpack_args_gcc(&vm, "HIIBHBIHBB", &a, &b, &c, &d, &e, &f, &g, &h, &i, &j);
-
-            printf("a = %d "
-                   "b = %d "
-                   "c = %d "
-                   "d = %d "
-                   "e = %d "
-                   "f = %d "
-                   "g = %d "
-                   "h = 0x%x "
-                   "i = %d"
-                   "j = %d\n", a, b, c, d, e, f, g, h, i, j);
-        }
-        break;
-
-    case 4:
-        {
-            static uint8_t cnt = 0;
-            avrvm_pack_return_gcc(&vm, 'B', &cnt);
-            cnt++;
-        }
-        break;
-
-    case 5:
-        {
-            uint16_t p;
-            avrvm_unpack_args_gcc(&vm, "P", &p);
-            printf("%s", &sram[p]);
-        }
-        break;
-
-//double (* const hostapi_mirror_d)(double) = (void *)(HOSTAPI_BASE + 13);
-//int64_t (* const hostapi_mirror_q)(int64_t) = (void *)(HOSTAPI_BASE + 14);
-//uint64_t (* const hostapi_mirror_Q)(uint64_t) = (void *)(HOSTAPI_BASE + 15);
-//void * (* const hostapi_mirror_p)(void *) = (void *)(HOSTAPI_BASE + 15);
-
-
-    case 6:
-        {
-            char c;
-            avrvm_unpack_args_gcc(&vm, "c", &c);
-            avrvm_pack_return_gcc(&vm, 'c', &c);
-        }
-        break;
-
-    case 7:
-        {
-            int8_t b;
-            avrvm_unpack_args_gcc(&vm, "b", &b);
-            avrvm_pack_return_gcc(&vm, 'b', &b);
-        }
-        break;
-
-    case 8:
-        {
-            uint8_t B;
-            avrvm_unpack_args_gcc(&vm, "B", &B);
-            avrvm_pack_return_gcc(&vm, 'B', &B);
-        }
-        break;
-
-    case 9:
-        {
-            int16_t h;
-            avrvm_unpack_args_gcc(&vm, "h", &h);
-            avrvm_pack_return_gcc(&vm, 'h', &h);
-        }
-        break;
-
-    case 10:
-        {
-            uint16_t H;
-            avrvm_unpack_args_gcc(&vm, "H", &H);
-            avrvm_pack_return_gcc(&vm, 'H', &H);
-        }
-        break;
-
-    case 11:
-        {
-            int32_t i;
-            avrvm_unpack_args_gcc(&vm, "i", &i);
-            avrvm_pack_return_gcc(&vm, 'i', &i);
-        }
-        break;
-
-    case 12:
-        {
-            uint32_t I;
-            avrvm_unpack_args_gcc(&vm, "I", &I);
-            avrvm_pack_return_gcc(&vm, 'I', &I);
-        }
-        break;
-
-    case 13:
-        {
-            float d;
-            avrvm_unpack_args_gcc(&vm, "f", &d);
-            avrvm_pack_return_gcc(&vm, 'f', &d);
-        }
-        break;
-
-    case 14:
-        {
-            int64_t q;
-            avrvm_unpack_args_gcc(&vm, "q", &q);
-            avrvm_pack_return_gcc(&vm, 'q', &q);
-        }
-        break;
-
-    case 15:
-        {
-            uint32_t Q;
-            avrvm_unpack_args_gcc(&vm, "Q", &Q);
-            avrvm_pack_return_gcc(&vm, 'Q', &Q);
-        }
-        break;
-
-    case 16:
-        {
-            uint16_t p;
-            avrvm_unpack_args_gcc(&vm, "P", &p);
-            avrvm_pack_return_gcc(&vm, 'P', &p);
-        }
-        break;
-
-
-    default:
-        #warning "special spoil"
-        vm.regs[24] ^= 0xFF;
-        vm.regs[25] ^= 0xFF;
         break;
     }
 }
 
-static void run_hello(const char *fname)
+int main(int argc, const char *argv[])
 {
+    if (argc < 2) return 0;
+
     avrvm_iface_t iface =
     {
         .flash_r = flash_reader,
@@ -251,19 +96,17 @@ static void run_hello(const char *fname)
     };
 
     avrvm_init(&vm, &iface, sizeof(sram));
-    src = fopen(fname, "rb");
+    src = fopen(argv[1], "rb");
     if (!src)
     {
         printf("failed to open");
-        return;
+        return -1;
     }
 
     while (1)
     {
         if (_kbhit())
-        {
             io[0] = _getch();   // move key code to 'KEYB_IO register'
-        }
 
         int rc = avrvm_exec(&vm);
         if (rc == AVRVM_RC_OK)
@@ -276,25 +119,17 @@ static void run_hello(const char *fname)
         else if (rc == AVRVM_RC_UNDEF_INSTR)
         {
             printf("bad instruction");
-            exit(-1);
+            return -1;
         }
         else if (rc == AVRVM_RC_BREAK)
         {
             printf("end of script");
-            exit(0);
+            return 0;
         }
         else if (rc == AVRVM_RC_SLEEP)
         {
             printf("sleep request");
         }
     }
-}
-
-int main(int argc, const char *argv[])
-{
-    if (argc < 2) return 0;
-    run_hello(argv[1]);
-
-    return 0;
 }
 
